9912dea12e3dc6a0333135830d8e19a29f5689db
nathan.seville <nathan.seville@hotmail.ch>
2019-03-11 10:40:34 +0100

    I've implemented the FileNumberingFilterWriter

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/FileNumberingFilterWriter.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/FileNumberingFilterWriter.java
@@ -3,6 +3,7 @@ package ch.heigvd.res.labio.impl.filters;
 import java.io.FilterWriter;
 import java.io.IOException;
 import java.io.Writer;
+import java.util.Objects;
 import java.util.logging.Logger;
 
 /**
@@ -19,23 +20,58 @@ public class FileNumberingFilterWriter extends FilterWriter {
 
   private static final Logger LOG = Logger.getLogger(FileNumberingFilterWriter.class.getName());
 
+  private int line = 0;
+  private boolean[] newline = {true, true};
+
   public FileNumberingFilterWriter(Writer out) {
     super(out);
   }
 
   @Override
   public void write(String str, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    //Objects.checkFromIndexSize(off, len, str.length()); // @since 9 (can replace next two lines)
+
+    if ((str.length() | off | len) < 0 || len > str.length() - off)
+      throw new IndexOutOfBoundsException();
+
+    for(int i = off; i < len + off; ++i)
+      this.write(str.charAt(i));
   }
 
   @Override
   public void write(char[] cbuf, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    //Objects.checkFromIndexSize(off, len, cbuf.length); // @since 9 (can replace next two lines)
+
+    if ((cbuf.length | off | len) < 0 || len > cbuf.length - off)
+      throw new IndexOutOfBoundsException();
+
+    for(int i = off; i < len + off; ++i)
+      this.write(cbuf[i]);
   }
 
   @Override
   public void write(int c) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
+    if(c == '\n' || c == '\r') {
+      newline[0] = true;
+      newline[1] = false;
 
+      if(c == '\n') {
+        newline[1] = true;
+        super.write(c);
+      }
+    } else
+      newline[1] = true;
+
+    if(newline[0] && newline[1]) {
+      String str = ++line + "\t";
+      super.write(str, 0, str.length());
+
+      newline[0] = false;
+
+      // si le caractere n'est pas un retour mais que newline est à true c'est la premiere ligne
+      if(c != '\n' && c != '\r')
+        super.write(c);
+    } else
+      super.write(c);
+  }
 }
